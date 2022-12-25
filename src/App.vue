f6bddd699b0b7f6a6745551caf333bfb23548ce6
Eugene848 <m204isachenko@gmail.com>
2022-12-25 13:48:22 +0300

    One_message

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,60 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+
+<MyContainer>
+<ChatWindow>
+<ChatMessage  v-bind:username="username" v-bind:time="time">
+  {{message}}
+</ChatMessage>
+</ChatWindow>
+</MyContainer>
+
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+
+import MyContainer from "./components/MyContainer.vue";
+import ChatMessage from "./components/ChatMessage.vue";
+import ChatWindow from "./components/ChatWindow.vue";
 
 export default {
-  name: 'App',
+  name: "App",
   components: {
-    HelloWorld
+    MyContainer,
+    ChatMessage,
+    ChatWindow
+  },
+  data(){
+  return {
+    time: [],
+    username: [],
+    message: []
   }
-}
+  },
+  methods: {
+    Get_Mes(){
+      this.axios.get('https://61bcd385d8542f0017824a2a.mockapi.io/messages')
+        .then((response) => {
+          console.log(response.data)
+          for (let i = 0; i < 71; i++) {
+            this.time[i] = response.data[i]['datetime']
+            this.username[i] = response.data[i]['author']
+            this.message[i] = response.data[i]['text']
+          }
+        })
+    }
+  },
+  mounted() {
+    this.Get_Mes();
+  }
+};
+
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+
+body {
+  margin: 0;
+  background-color: #f9f9fa;
 }
+
 </style>
